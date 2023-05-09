f60580d0025f6cfe14f11ecd9c4d8c05e4b9fdfc
NotBad5 <122005188+NotBad5@users.noreply.github.com>
2023-05-09 19:40:07 +0200

    Update cs.lua

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,6 +1,6 @@
 Locales['cs'] = {
 	['inZone'] = 'Nacházíte se v oblasti loveckého revíru, je zde zvyšený výskyt divoké zvěře',
 	['outOfTheZone'] = 'Opustili jste oblast loveckého revíru!',
-	[''] = '',
+	['pressE'] = 'Stiskni ~g~[E]~w~ pro vykuchaní zvírete',
 	['animalProcessed'] = 'Zpracoval jsi zvíře.',
 }
