6fe853eb395a9ccf53d6e60a48b572ed3a3375dc
NotBad5 <122005188+NotBad5@users.noreply.github.com>
2023-05-09 19:40:19 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,6 @@
 Locales['en'] = {
 	['inZone'] = 'You are in the area of ​​a hunting ground, there is an increased occurrence of wild animals.',
 	['outOfTheZone'] = 'You have left the hunting ground area!',
-	[''] = '',
+	['pressE'] = 'Press ~g~[E]~w~ to process animal',
 	['animalProcessed'] = 'You have processed an animal.',
 }
